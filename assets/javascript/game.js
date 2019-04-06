4de1f2877eaebb2f2c4399700bb9dcfacd879056
Juan Alberto Pimentel JR <jpimentel45@gmail.com>
2019-04-06 09:34:52 -0700

    Fixed my error with JS code: it was talling each selection as a loss, after the first win/loss. Named elements, and variables the same, causing my values to remain the same, added some css styling.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,108 +1,83 @@
 $(document).ready(function () {
     //Set up variables
-    var generatedNum = "";
     var wins = 0;
     var losses = 0;
-    var crystal1 = "";
-    var crystal2 = "";
-    var crystal3 = "";
-    var crystal4 = "";
-    var totalScore = 0;
-
-    //4 crystals displayed as buttons, one box containning random num
-    //create random generator for: generatedNum:(19-120), and crystal buttons(1-12)
-    // crystal random: var .click = Math.floor((Math.random() * 12) + 1);
-    // generatedNum: var .click = Math.floor((Math.random() * 120) + 19);
+    var newtotalScore = 0;
+    $(".wins").text("Wins: " + wins);
+    $(".losses").text("Losses: " + losses);
+    var newgeneratedNum = Math.floor((Math.random() * 101) + 19);
+    $(".generatedNum").text(newgeneratedNum);
+    //console.log(generatedNum);
     var crystalNum1 = Math.floor((Math.random() * 12) + 1);
     var crystalNum2 = Math.floor((Math.random() * 12) + 1);
     var crystalNum3 = Math.floor((Math.random() * 12) + 1);
     var crystalNum4 = Math.floor((Math.random() * 12) + 1);
-    var generatedNum = Math.floor((Math.random() * 120) + 19);
-
-    console.log(generatedNum);
-    //random num shown at start of game (function(initializeGame Might have taken care of this arleady))
-    $(".generatedNum").text(generatedNum);
-    $(".wins").text("Wins: " + wins);
-    $(".losses").text("Losses: " + losses);
 
     function initializeGame() {
-
-        var crystalNum1 = Math.floor((Math.random() * 12) + 1);
-        var crystalNum2 = Math.floor((Math.random() * 12) + 1);
-        var crystalNum3 = Math.floor((Math.random() * 12) + 1);
-        var crystalNum4 = Math.floor((Math.random() * 12) + 1);
-        var generatedNum = Math.floor((Math.random() * 120) + 19);
-        $(".generatedNum").text(generatedNum);
-        var wins = 0;
-        var losses = 0;
-        var totalScore = 0;
-        $(".totalScore").text(totalScore);
-        $("#wins, #losses, #totalScore").empty();
-        //document.getElementsByClassName(".generatedNum").innerHTML = generatedNum;
-
+        newgeneratedNum = Math.floor((Math.random() * 101) + 19);
+        $(".generatedNum").text(newgeneratedNum);
+        crystalNum1 = Math.floor((Math.random() * 12) + 1);
+        crystalNum2 = Math.floor((Math.random() * 12) + 1);
+        crystalNum3 = Math.floor((Math.random() * 12) + 1);
+        crystalNum4 = Math.floor((Math.random() * 12) + 1);
+        newtotalScore = 0;
+        $(".totalScore").text(newtotalScore);
     }
-
-    // wins { totalScore === generatedNum}
+    // wins { totalScore === newgeneratedNum}
     function win() {
         wins++;
         $(".wins").text("Wins: " + wins);
         initializeGame();
     }
-    // losses {totalScore > generatedNum}
+    // losses {totalScore > newgeneratedNum}
     function loss() {
         losses++;
         $(".losses").text("Losses: " + losses);
         initializeGame();
     }
-
-    // add $().on("click", Function()) for when player clicks on crystal
-    //Value will be hidden; player will know value when click and add value to total score
+    //on click function for crystals
     $(".crystal1").click(function () {
-        totalScore = totalScore + crystalNum1;
-        console.log(" New totalScore= " + totalScore);
-        $(".totalScore").text(totalScore);
-        if (totalScore === generatedNum) {
+        newtotalScore = newtotalScore + crystalNum1;
+        console.log(" New totalScore= " + newtotalScore);
+        $(".totalScore").text(newtotalScore);
+
+        if (newtotalScore === newgeneratedNum) {
             win();
-        } else if (totalScore > generatedNum) {
+        } else if (newtotalScore > newgeneratedNum) {
             loss();
         }
     });
     $(".crystal2").click(function () {
-        totalScore = totalScore + crystalNum2;
-        console.log(" New totalScore= " + totalScore);
-        $(".totalScore").text(totalScore);
-        if (totalScore === generatedNum) {
+        newtotalScore = newtotalScore + crystalNum2;
+        console.log(" New totalScore= " + newtotalScore);
+        $(".totalScore").text(newtotalScore);
+
+        if (newtotalScore === newgeneratedNum) {
             win();
-        } else if (totalScore > generatedNum) {
+        } else if (newtotalScore > newgeneratedNum) {
             loss();
         }
     });
     $(".crystal3").click(function () {
-        totalScore = totalScore + crystalNum3;
-        console.log(" New totalScore= " + totalScore);
-        $(".totalScore").text(totalScore);
-        if (totalScore === generatedNum) {
+        newtotalScore = newtotalScore + crystalNum3;
+        console.log(" New totalScore= " + newtotalScore);
+        $(".totalScore").text(newtotalScore);
+
+        if (newtotalScore === newgeneratedNum) {
             win();
-        } else if (totalScore > generatedNum) {
+        } else if (newtotalScore > newgeneratedNum) {
             loss();
         }
     });
     $(".crystal4").click(function () {
-        totalScore = totalScore + crystalNum4;
-        console.log(" New totalScore= " + totalScore);
-        $(".totalScore").text(totalScore);
-        if (totalScore === generatedNum) {
+        newtotalScore = newtotalScore + crystalNum4;
+        console.log(" New totalScore= " + newtotalScore);
+        $(".totalScore").text(newtotalScore);
+
+        if (newtotalScore === newgeneratedNum) {
             win();
-        } else if (totalScore > generatedNum) {
+        } else if (newtotalScore > newgeneratedNum) {
             loss();
         }
     });
-    //Add crystals random value added to players total score
-
-
-
-    //restatrt game when player wins/loses
-    //new game = new generatedNum, 4 new hidden crystal values, users score, score counter reset to zero
-
-
 });
---
+++ assets/javascript/test.js
@@ -0,0 +1,83 @@
+$(document).ready(function () {
+    //Set up variables
+    var wins = 0;
+    var losses = 0;
+    var newtotalScore = 0;
+    $(".wins").text("Wins: " + wins);
+    $(".losses").text("Losses: " + losses);
+    var newgeneratedNum = Math.floor((Math.random() * 101) + 19);
+    $(".generatedNum").text(newgeneratedNum);
+    //console.log(generatedNum);
+    var crystalNum1 = Math.floor((Math.random() * 12) + 1);
+    var crystalNum2 = Math.floor((Math.random() * 12) + 1);
+    var crystalNum3 = Math.floor((Math.random() * 12) + 1);
+    var crystalNum4 = Math.floor((Math.random() * 12) + 1);
+
+    function initializeGame() {
+        newgeneratedNum = Math.floor((Math.random() * 120) + 19);
+        $(".generatedNum").text(newgeneratedNum);
+        crystalNum1 = Math.floor((Math.random() * 12) + 1);
+        crystalNum2 = Math.floor((Math.random() * 12) + 1);
+        crystalNum3 = Math.floor((Math.random() * 12) + 1);
+        crystalNum4 = Math.floor((Math.random() * 12) + 1);
+        newtotalScore = 0;
+        $(".totalScore").text(newtotalScore);
+    }
+    // wins { totalScore === newgeneratedNum}
+    function win() {
+        wins++;
+        $(".wins").text("Wins: " + wins);
+        initializeGame();
+    }
+    // losses {totalScore > newgeneratedNum}
+    function loss() {
+        losses++;
+        $(".losses").text("Losses: " + losses);
+        initializeGame();
+    }
+    //on click function for crystals
+    $(".crystal1").click(function () {
+        newtotalScore = newtotalScore + crystalNum1;
+        console.log(" New totalScore= " + newtotalScore);
+        $(".totalScore").text(newtotalScore);
+
+        if (newtotalScore === newgeneratedNum) {
+            win();
+        } else if (newtotalScore > newgeneratedNum) {
+            loss();
+        }
+    });
+    $(".crystal2").click(function () {
+        newtotalScore = newtotalScore + crystalNum2;
+        console.log(" New totalScore= " + newtotalScore);
+        $(".totalScore").text(newtotalScore);
+
+        if (newtotalScore === newgeneratedNum) {
+            win();
+        } else if (newtotalScore > newgeneratedNum) {
+            loss();
+        }
+    });
+    $(".crystal3").click(function () {
+        newtotalScore = newtotalScore + crystalNum3;
+        console.log(" New totalScore= " + newtotalScore);
+        $(".totalScore").text(newtotalScore);
+
+        if (newtotalScore === newgeneratedNum) {
+            win();
+        } else if (newtotalScore > newgeneratedNum) {
+            loss();
+        }
+    });
+    $(".crystal4").click(function () {
+        newtotalScore = newtotalScore + crystalNum4;
+        console.log(" New totalScore= " + newtotalScore);
+        $(".totalScore").text(newtotalScore);
+
+        if (newtotalScore === newgeneratedNum) {
+            win();
+        } else if (newtotalScore > newgeneratedNum) {
+            loss();
+        }
+    });
+});
